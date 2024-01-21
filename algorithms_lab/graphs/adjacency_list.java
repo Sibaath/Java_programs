71cb1651c1b0b2b166d971d46c588bb91bd86469
Sibaath <sibaathahmed2004@gmail.com>
2024-01-21 17:36:08 +0530

    completed

---
+++ algorithms_lab/graphs/adjacency_list.java
@@ -0,0 +1,46 @@
+package algorithms_lab.graphs;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class adjacency_list {
+    public static void make_edge(ArrayList<ArrayList<Integer>> vertex_list,int source,int destination,int type)
+    {
+        if(type==1)
+            vertex_list.get(destination-1).add(source);
+        vertex_list.get(source-1).add(destination);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the type of graph [Directed/undirected](0/1) : ");
+        int type = sc.nextInt();
+        System.out.print("Enter the number of vertices : ");
+        int vertices = sc.nextInt();
+        ArrayList<ArrayList<Integer>> vertex_list = new ArrayList<>();
+        for(int i = 0;i<vertices;i++)
+        vertex_list.add(new ArrayList<>());
+        System.out.print("Enter the number of edges : ");
+        int edges = sc.nextInt();
+        for(int i = 0 ;i<edges;i++)
+        {
+            System.out.print("Enter the souce vertex for edge "+(i+1)+" : ");
+            int source = sc.nextInt();
+            System.out.print("Enter the Destination vertex for edge "+(i+1)+" : ");
+            int destination = sc.nextInt();
+            make_edge(vertex_list, source, destination,type);
+        }
+        System.out.println("The matrix representation is : ");
+        for(int i =0;i<vertices;i++)
+        {
+            System.out.print(i+1+"->");
+            for(int j = 0 ;j<vertex_list.get(i).size();j++)
+            {
+                System.out.print(vertex_list.get(i).get(j)+"->");
+            }
+            System.out.println("nil");
+            System.out.println();
+        }
+        sc.close();
+    }
+}
---
+++ algorithms_lab/graphs/matrix_rep.java
@@ -0,0 +1,38 @@
+package algorithms_lab.graphs;
+import java.util.*;
+public class matrix_rep {
+    public static void make_edge(int[][] matrix,int source,int destination,int type)
+    {
+        if(type==1)
+            matrix[destination-1][source-1] = 1;
+        matrix[source-1][destination-1] = 1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the type of graph [Directed/undirected](0/1) : ");
+        int type = sc.nextInt();
+        System.out.print("Enter the number of vertices : ");
+        int vertices = sc.nextInt();
+        int[][] matrix = new int[vertices][vertices];
+        System.out.print("Enter the number of edges : ");
+        int edges = sc.nextInt();
+        for(int i = 0 ;i<edges;i++)
+        {
+            System.out.print("Enter the souce vertex for edge "+(i+1)+" : ");
+            int source = sc.nextInt();
+            System.out.print("Enter the Destination vertex for edge "+(i+1)+" : ");
+            int destination = sc.nextInt();
+            make_edge(matrix,source,destination,type);
+        }
+        System.out.println("The matrix representation is : ");
+        for(int i = 0;i<vertices;i++)
+        {
+            for(int j =0;j<vertices ;j++)
+            {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
